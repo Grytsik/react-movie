4034bc873e68e43b4e3cb5449bde744db231a3f0
Grytsik <gryyytsik@gmai.com>
2024-01-11 11:02:59 +0200

    commit

--- src/pages/SelectMovieCard/SelectMovieLike.jsx
+++ src/pages/SelectMovieCard/SelectMovieLike.jsx
@@ -23,14 +23,7 @@ export default function SelectMovieLike({ addLikeMovie, isLiked, id, loading }) 
     };
   }, []);
 
-  const handleButtonClick = () => {
-    scroll.scrollTo('trailer', {
-      smooth: true,
-      duration: 500,
-      offset: offset,
-      horizontal: true,
-    });
-  };
+  
 
   return (
     <div className='selectMovieCard__like'>
